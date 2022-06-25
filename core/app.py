ad07361f9a9cfe090f4e44a01a0e9ded21f54849
GarnikOriginal <gavid19912@gmail.com>
2022-06-25 16:23:11 +0500

    Fix connection to Minio

--- core/app.py
+++ core/app.py
@@ -1,9 +1,10 @@
 import os
 import logging
+import boto3
+from botocore.client import Config
 from aiohttp.web import Application
 from dotenv import load_dotenv, find_dotenv
 from sqlalchemy.ext.asyncio import create_async_engine
-from aiohttp_jwt import JWTMiddleware
 from core.routes import setup_routes
 
 
@@ -12,9 +13,7 @@ async def create_app():
         logging.warning("Cant find .env file.")
     load_dotenv()
 
-    app = Application(middlewares=[
-        JWTMiddleware(os.environ.get("JWT_KEY")),
-    ])
+    app = Application()
     app = setup_routes(app)
     connection_string = f"postgresql+asyncpg://" \
                         f"{os.environ.get('DB_USER')}:%s" % os.environ.get('DB_PASSWORD') + \
@@ -25,4 +24,16 @@ async def create_app():
         max_overflow=int(os.environ.get('POOL_OVERFLOW')),
         pool_recycle=int(os.environ.get('POOL_RECYCLE'))
     )
+    app["jwt_key"] = os.environ.get('JWT_KEY')
+    app["jwt_alg"] = os.environ.get('JWT_ALG')
+    app["worker_host"] = os.environ.get('WORKER_HOST')
+    app["minio"] = boto3.resource(
+        's3',
+        endpoint_url=os.environ.get("MINIO_ENDPOINT"),
+        aws_access_key_id=os.environ.get("MINIO_ACCESS"),
+        aws_secret_access_key=os.environ.get("MINIO_SECRET"),
+        verify=False,
+        region_name="ru-msk-1"
+    )
+    app["minio_bucket"] = app["minio"].Bucket(os.environ.get("MINIO_BUCKET"))
     return app
