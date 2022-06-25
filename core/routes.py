12076d11e0f8fbfdf212a6643505242cff755c2e
GarnikOriginal <gavid19912@gmail.com>
2022-06-25 23:06:05 +0500

    Add tag groups route

--- core/routes.py
+++ core/routes.py
@@ -1,15 +1,25 @@
+import aiohttp_cors
 from aiohttp.web import Application
 from endpoints.tag import Tag, TagDeleter
-from endpoints.image import Image, Images, ImageDeleter
+from endpoints.image import Image, Images, ImagesSearch, ImageDeleter
 from endpoints.tag_group import TagGroup, TagGroups
 
 
 def setup_routes(app: Application):
     app.router.add_view("/image", Image)
     app.router.add_view("/images", Images)
+    app.router.add_view("/images/search", ImagesSearch)
     app.router.add_view("/image/delete", ImageDeleter)
     app.router.add_view("/tag", Tag)
     app.router.add_post("/tag/delete", TagDeleter)
     app.router.add_view("/tag_group", TagGroup)
     app.router.add_view("/tag_groups", TagGroups)
+    # Configure default CORS settings.
+    cors = aiohttp_cors.setup(app, defaults={
+        "*": aiohttp_cors.ResourceOptions(
+            allow_credentials=True,
+            expose_headers="*",
+            allow_headers="*",
+        )
+    })
     return app
--- endpoints/tag_group.py
+++ endpoints/tag_group.py
@@ -1,6 +1,6 @@
 from aiohttp import web
 from aiohttp_pydantic import PydanticView
-from db.db_tag_group import insert_tag_group, get_tag_group
+from db.db_tag_group import insert_tag_group, get_tag_group, get_all
 from db.db_tag import get_tags_by_group
 
 
@@ -42,21 +42,14 @@ class TagGroup(PydanticView):
 class TagGroups(PydanticView):
     async def get(self):
         try:
-            """
-            group = await get_tag_group(self.request.app, group_id)
-            tags = await get_tags_by_group(self.request.app, group_id)
-            return web.json_response({
-                "group": {
-                    "id": group[],
-                    "name": group["name"],
-                    "binary": group["binary"],
-                },
-                "tags": [{"id": tag["id"],
-                          "name": tag["name"],
-                          "text": tag["text"],
-                          "latent_space": tag["latent_space"]
-                          } for tag in tags]
-            }, status=200)
-            """
+            groups = await get_all(self.request.app)
+            result = {}
+            for g_id, g_name, g_binary, t_id, t_name, t_text, t_latent_space, t_is_fake in groups:
+                tag = {"id": t_id, "name": t_name, "text": t_text, "fake": t_is_fake}
+                if g_id in result.keys():
+                    result[g_id]["tags"].append(tag)
+                else:
+                    result[g_id] = {"group": {"id": g_id, "name": g_name, "binary": g_binary}, "tags": [tag]}
+            return web.json_response(list(result.values()), status=200)
         except Exception as err:
-            web.json_response({"Error": err}, status=500)
+            return web.json_response({"Error": err}, status=500)
